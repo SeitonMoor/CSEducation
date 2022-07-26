deca701ea524c4ee7962857209c26bab8c4c3af7
Anton Kovalenko <anton.saha@mail.ru>
2022-07-26 21:07:53 +0300

    The crystal store has been created

---
+++ CSEducation/CristalsShop.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSEducation
+{
+    internal class CristalsShop
+    {
+        public void BuyCristals()
+        {
+            int crystalPrice = 100;
+
+            Console.Write("Какое у вас количество золота: ");
+            int.TryParse(Console.ReadLine(), out int userGold);
+            Console.WriteLine("Добро пожаловать в магазин кристаллов!");
+            Console.WriteLine($"Какое количество кристалов вы хотите приобрести? Цена 1 кристалла: {crystalPrice}");
+            Console.Write("Ваш ответ: ");
+            int.TryParse(Console.ReadLine(), out int purchaseRequest);
+
+            userGold -= (purchaseRequest * crystalPrice);
+            int userCrystals = purchaseRequest;
+
+            Console.WriteLine("Поздравляю с покупкой!");
+            Console.WriteLine($"Теперь у вас: {userGold} золота и {userCrystals} кристаллов");
+        }
+    }
+}
--- CSEducation/Program.cs
+++ CSEducation/Program.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CSEducation
+﻿namespace CSEducation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            SwappingValues swappingValues = new SwappingValues();
-            swappingValues.Swapping();
+            CristalsShop cristalsShop = new CristalsShop();
+            cristalsShop.BuyCristals();
         }
     }
 }
