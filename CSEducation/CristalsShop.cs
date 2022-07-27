d5da90355cef91ed1806f811f7a4ac050a09552f
Anton Kovalenko <anton.saha@mail.ru>
2022-07-27 12:25:43 +0300

    Inputs validation changed according to learned modules

--- CSEducation/CristalsShop.cs
+++ CSEducation/CristalsShop.cs
@@ -6,40 +6,22 @@ namespace CSEducation
     {
         static void Main(string[] args)
         {
-            int userGold;
-            int userCrystals;
-            int purchaseRequest;
-            bool isValidInput;
             int crystalPrice = 100;
 
             Console.Write("Какое у вас количество золота: ");
-            isValidInput = int.TryParse(Console.ReadLine(), out userGold);
+            int userGold = Convert.ToInt32(Console.ReadLine());
 
-            if (!isValidInput)
-            {
-                Console.WriteLine("Необходимо ввести количество золота - числом.");
-            }
-            else
-            {
-                Console.WriteLine("Добро пожаловать в магазин кристаллов!");
-                Console.WriteLine($"Какое количество кристалов вы хотите приобрести? Цена 1 кристалла: {crystalPrice}");
+            Console.WriteLine("Добро пожаловать в магазин кристаллов!");
+            Console.WriteLine($"Какое количество кристалов вы хотите приобрести? Цена 1 кристалла: {crystalPrice}");
 
-                Console.Write("Ваш ответ: ");
-                isValidInput = int.TryParse(Console.ReadLine(), out purchaseRequest);
+            Console.Write("Ваш ответ: ");
+            int purchaseRequest = Convert.ToInt32(Console.ReadLine());
 
-                if (!isValidInput)
-                {
-                    Console.WriteLine("Необходимо ввести количество кристаллов - числом.");
-                }
-                else
-                {
-                    userGold -= (purchaseRequest * crystalPrice);
-                    userCrystals = purchaseRequest;
+            userGold -= (purchaseRequest * crystalPrice);
+            int userCrystals = purchaseRequest;
 
-                    Console.WriteLine("Поздравляю с покупкой!");
-                    Console.WriteLine($"Теперь у вас: {userGold} золота и {userCrystals} кристаллов");
-                }
-            }
+            Console.WriteLine("Поздравляю с покупкой!");
+            Console.WriteLine($"Теперь у вас: {userGold} золота и {userCrystals} кристаллов");
         }
     }
 }
