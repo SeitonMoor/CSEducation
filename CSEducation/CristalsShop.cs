3f97e06b0843ddaed6ffe7a2476c4d600d6e4fbb
Anton Kovalenko <anton.saha@mail.ru>
2022-07-27 10:44:33 +0300

    Added inputs validation in the crystal store

--- CSEducation/CristalsShop.cs
+++ CSEducation/CristalsShop.cs
@@ -6,17 +6,41 @@ namespace CSEducation
     {
         static void Main(string[] args)
         {
+            int userGold;
+            int userCrystals;
+            int purchaseRequest;
+            bool isValidInput;
             int crystalPrice = 100;
 
-            Console.Write("Какое у вас количество золота: ");
-            int.TryParse(Console.ReadLine(), out int userGold);
+            do
+            {
+                Console.Write("Какое у вас количество золота: ");
+                isValidInput = int.TryParse(Console.ReadLine(), out userGold);
+
+                if (!isValidInput)
+                {
+                    Console.WriteLine("Необходимо ввести количество золота - числом.");
+                }
+            }
+            while (!isValidInput);
+
             Console.WriteLine("Добро пожаловать в магазин кристаллов!");
             Console.WriteLine($"Какое количество кристалов вы хотите приобрести? Цена 1 кристалла: {crystalPrice}");
-            Console.Write("Ваш ответ: ");
-            int.TryParse(Console.ReadLine(), out int purchaseRequest);
+
+            do
+            {
+                Console.Write("Ваш ответ: ");
+                isValidInput = int.TryParse(Console.ReadLine(), out purchaseRequest);
+
+                if (!isValidInput)
+                {
+                    Console.WriteLine("Необходимо ввести количество кристаллов - числом.");
+                }
+            }
+            while (!isValidInput);
 
             userGold -= (purchaseRequest * crystalPrice);
-            int userCrystals = purchaseRequest;
+            userCrystals = purchaseRequest;
 
             Console.WriteLine("Поздравляю с покупкой!");
             Console.WriteLine($"Теперь у вас: {userGold} золота и {userCrystals} кристаллов");
