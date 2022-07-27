a877aa4f15c7ff3cf3a74130cf24c79a23372499
Anton Kovalenko <anton.saha@mail.ru>
2022-07-27 12:45:02 +0300

    Polyclinic with calculating queue waiting time created

---
+++ CSEducation/Polyclinic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSEducation
+{
+    internal class Polyclinic
+    {
+        static void Main(string[] args)
+        {
+            int timePerPatient = 10;
+
+            Console.Write("Напишите какое количество людей в очереди: ");
+            int patientsInQueue = Convert.ToInt32(Console.ReadLine());
+
+            int hoursWaiting = patientsInQueue * timePerPatient / 60;
+            int minutesWaiting = patientsInQueue * timePerPatient % 60;
+
+            Console.WriteLine($"Вы должны отстоять в очереди {hoursWaiting} часа и {minutesWaiting} минут.");
+        }
+    }
+}
