9cfd23b10f657514a4aac900e1917e3b7308c0af
Anton Kovalenko <anton.saha@mail.ru>
2022-07-27 12:55:31 +0300

    Calculating queue waiting time without magic numbers

--- CSEducation/Polyclinic.cs
+++ CSEducation/Polyclinic.cs
@@ -7,12 +7,14 @@ namespace CSEducation
         static void Main(string[] args)
         {
             int timePerPatient = 10;
+            int minutesInHour = 60;
 
             Console.Write("Напишите какое количество людей в очереди: ");
             int patientsInQueue = Convert.ToInt32(Console.ReadLine());
 
-            int hoursWaiting = patientsInQueue * timePerPatient / 60;
-            int minutesWaiting = patientsInQueue * timePerPatient % 60;
+            int timeWaiting = patientsInQueue * timePerPatient;
+            int hoursWaiting = timeWaiting / minutesInHour;
+            int minutesWaiting = timeWaiting % minutesInHour;
 
             Console.WriteLine($"Вы должны отстоять в очереди {hoursWaiting} часа и {minutesWaiting} минут.");
         }
