2cf5f20c2efc60202ac786426ca183f41085e82b
Anton Kovalenko <anton.saha@mail.ru>
2022-07-30 19:48:42 +0300

    Created control of the exit by the cycle "while"

---
+++ ConditionalOperatorsAndLoops/OutputControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConditionalOperatorsAndLoops
+{
+    internal class OutputControl
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            bool canExit = false;
+
+            while (!canExit)
+            {
+                Console.WriteLine("Какое действие хотите совершить: create | update | delete | exit");
+                userInput = Console.ReadLine();
+
+                if (userInput == "exit")
+                {
+                    canExit = true;
+                }
+                else
+                {
+                    Console.WriteLine("Выполнение программы...");
+                }
+            }
+        }
+    }
+}
