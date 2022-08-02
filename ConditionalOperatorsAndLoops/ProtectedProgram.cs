48bb737a97738bf9400c7bfbeb942f826f68d1bb
Anton Kovalenko <anton.saha@mail.ru>
2022-08-02 18:15:19 +0300

    Password-protected program has been created

---
+++ ConditionalOperatorsAndLoops/ProtectedProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConditionalOperatorsAndLoops
+{
+    internal class ProtectedProgram
+    {
+        static void Main(string[] args)
+        {
+            string password = "t0pSecret";
+            int tryCount = 3;
+            string userInput;
+
+            for (int i = 1; i <= tryCount; i++)
+            {
+                Console.Write("Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (password == userInput)
+                {
+                    Console.WriteLine("\nДоступ к тайному сообщению получен!\n");
+                    Console.WriteLine("Тайное сообщение ...");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"У вас осталось попыток: {tryCount - i}");
+                }
+            }
+        }
+    }
+}
