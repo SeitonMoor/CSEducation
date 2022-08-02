2c4de48cf4fabb4520c1ec4eee4f9efde2e9d66f
Anton Kovalenko <anton.saha@mail.ru>
2022-08-02 18:44:14 +0300

    A searcher has been created for the number of three-digit natural numbers that are multiples of N.

---
+++ ConditionalOperatorsAndLoops/MultiplesNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConditionalOperatorsAndLoops
+{
+    internal class MultiplesNumbers
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int maxRandomValue = 28;
+            int minNaturalNumber = 100;
+            int maxNaturalNumber = 999;
+            int count = 0;
+            int n = random.Next(maxRandomValue);
+
+            for (int i = minNaturalNumber; i <= maxNaturalNumber; i++)
+            {
+                int multipleNumber = i;
+                while (multipleNumber > 0)
+                {
+                    multipleNumber -= n;
+                }
+
+                if (multipleNumber == 0)
+                    count++;
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
