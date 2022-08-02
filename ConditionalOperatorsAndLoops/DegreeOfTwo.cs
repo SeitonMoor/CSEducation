77c27607a7db2dc65d82a68b2cf8772e755d6b1c
Anton Kovalenko <anton.saha@mail.ru>
2022-08-02 19:42:56 +0300

    The searcher has been created for the minimum degree of two exceeding the set number

---
+++ ConditionalOperatorsAndLoops/DegreeOfTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConditionalOperatorsAndLoops
+{
+    internal class DegreeOfTwo
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            uint number = 2;
+            int minDegree = 0;
+            int setNumber = random.Next();
+
+            uint numberInDegree = number;
+            while (setNumber >= numberInDegree)
+            {
+                numberInDegree *= number;
+                minDegree++;
+            } 
+
+            Console.WriteLine($"Число { setNumber }, { minDegree } степень и число { number } в найденной степени: { numberInDegree }.");
+        }
+    }
+}
