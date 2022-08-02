6578822bd92955faba18d37f88fc5806af94edf9
Anton Kovalenko <anton.saha@mail.ru>
2022-08-02 23:39:24 +0300

    The program has been created to work with brackets expressions

---
+++ ConditionalOperatorsAndLoops/BracketsExpression.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConditionalOperatorsAndLoops
+{
+    internal class BracketsExpression
+    {
+        static void Main(string[] args)
+        {
+            int maxBracketsDepth = 0;
+            int foundDepth = 0;
+            int openedBrackets = 0;
+            bool isCorrect = true;
+            bool haveClosedBracket = false;
+
+            Console.Write("Введите скобочное выражение: ");
+            string inputBrackets = Console.ReadLine();
+
+            foreach (var symbol in inputBrackets)
+            {
+                if (symbol == '(')
+                {
+                    if (haveClosedBracket && foundDepth > 0) foundDepth--;
+                    openedBrackets++;
+                }
+                else if (symbol == ')')
+                {
+                    foundDepth++;
+                    openedBrackets--;
+                    haveClosedBracket = true;
+                }
+                
+                if (openedBrackets < 0) isCorrect = false;
+
+                if (openedBrackets == 0 && maxBracketsDepth < foundDepth)
+                {
+                    maxBracketsDepth = foundDepth;
+                    foundDepth = 0;
+                }
+            }
+
+            if (openedBrackets == 0 && isCorrect)
+            {
+                Console.WriteLine($"“{ inputBrackets }” - строка корректная и максимум глубины равняется { maxBracketsDepth }.");
+            }
+            else
+            {
+                Console.WriteLine($"“{ inputBrackets }” - строка некорректная.");
+            }
+        }
+    }
+}
