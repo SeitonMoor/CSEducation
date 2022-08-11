e38f05bb5979e913e25cc30278b9b950e9f87053
Anton Kovalenko <anton.saha@mail.ru>
2022-08-11 12:32:37 +0300

    Сounting values in a sequence, not in the entire array

--- Arrays/SubarrayOfRepetitions.cs
+++ Arrays/SubarrayOfRepetitions.cs
@@ -15,7 +15,7 @@ namespace Arrays
             int foundNumber = 0;
             int foundQuantity = 0;
             int minValue = 1;
-            int maxValue = 100;
+            int maxValue = 10;
 
             Console.WriteLine("Исходный массив:");
             for (int i = 0; i < array.Length; i++)
@@ -27,22 +27,33 @@ namespace Arrays
             foreach (int checkNumber in array)
             {
                 int count = 0;
+                bool isInSequence = false;
                 foreach (int number in array)
                 {
-                    if (number == checkNumber)
+                    if (isInSequence && number == checkNumber)
                     {
                         count++;
                     }
-                }
+                    else if (isInSequence)
+                    {
+                        isInSequence = false;
+                        count = 0;
+                    }
+                    else if (number == checkNumber)
+                    {
+                        isInSequence = true;
+                        count++;
+                    }
 
-                if (foundQuantity < count)
-                {
-                    foundNumber = checkNumber;
-                    foundQuantity = count;
+                    if (foundQuantity < count)
+                    {
+                        foundNumber = checkNumber;
+                        foundQuantity = count;
+                    }
                 }
             }
 
-            Console.WriteLine($"\nЧисло {foundNumber} повторяется чаще всего, а именно {foundQuantity} раз подряд");
+            Console.WriteLine($"\nЧисло {foundNumber} повторяется {foundQuantity} раз подряд");
         }
     }
 }
