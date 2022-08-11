226a0146afb4d511523328dcbd2e9666a6f4b50a
Anton Kovalenko <anton.saha@mail.ru>
2022-08-11 12:04:44 +0300

    Created getting an array of words from a string with text

---
+++ Arrays/StringSplitter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class StringSplitter
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Напишите текст: ");
+            string inputText = Console.ReadLine();
+
+            string[] wordsArray = inputText.Split(' ');
+
+            foreach (string word in wordsArray)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
