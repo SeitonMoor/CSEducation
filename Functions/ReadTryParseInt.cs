95b22bb130f4a899ff9a1f2f380ddafa96aa3c16
Anton Kovalenko <anton.saha@mail.ru>
2022-08-15 14:26:11 +0300

    Created the function that requests a number from the user and tries to convert it to the int type

---
+++ Functions/ReadTryParseInt.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Functions
+{
+    internal class ReadTryParseInt
+    {
+        static void Main(string[] args)
+        {
+            GetNumber(out int inputNumber);
+
+            Console.WriteLine($"Вы ввели число: {inputNumber}");
+        }
+
+        static int GetNumber(out int inputNumber)
+        {
+            bool beParsed = false;
+            inputNumber = 0;
+
+            while (beParsed == false)
+            {
+                Console.Write("Введите число: ");
+                beParsed = int.TryParse(Console.ReadLine(), out inputNumber);
+            }
+
+            return inputNumber;
+        }
+    }
+}
