a314b398b69c6f210ae7082e870eaf5fdc10a05c
Anton Kovalenko <anton.saha@mail.ru>
2022-08-20 15:45:00 +0300

    Updated Dynamic Array to Advanced level using Collections

--- Collections/DynamicArrayAdvanced.cs
+++ Collections/DynamicArrayAdvanced.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -6,51 +7,80 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            int[] numbersArray = new int[0];
+            List<int> numbers = new List<int>();
             bool isWorking = true;
 
             while (isWorking)
             {
-                Console.Write("Перечень команд:" +
+                SelectCommand(ref isWorking, numbers);
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        static void SelectCommand(ref bool isWorking, List<int> numbers)
+        {
+            Console.Write("Перечень команд:" +
                     "\nsum - вывод суммы всех введенных чисел." +
                     "\nexit - выход из программы" +
-                    "\nЛибо введите число для добавления в массив." +
+                    "\nЛибо введите число для добавления." +
                     "\nВаше действие: ");
-                string userInput = Console.ReadLine();
+            string userInput = Console.ReadLine();
 
-                switch (userInput)
-                {
-                    case "sum":
-                        int sum = 0;
+            switch (userInput)
+            {
+                case "sum":
+                    GetAmount(numbers);
+                    break;
 
-                        for (int i = 0; i < numbersArray.Length; i++)
-                            sum += numbersArray[i];
+                case "exit":
+                    isWorking = false;
+                    Console.WriteLine("\nВыход из программы...");
+                    break;
 
-                        Console.WriteLine($"\nСумма введеных чисел: {sum}\n");
-                        break;
-
-                    case "exit":
-                        isWorking = false;
-                        break;
-
-                    default:
-                        int inputNumber = Convert.ToInt32(userInput);
-                        int newArrayLength = numbersArray.Length + 1;
-                        int lastArrayIndex = newArrayLength - 1;
-
-                        int[] tempNumbersArray = new int[newArrayLength];
-                        for (int i = 0; i < numbersArray.Length; i++)
-                        {
-                            tempNumbersArray[i] = numbersArray[i];
-                        }
-
-                        tempNumbersArray[lastArrayIndex] = inputNumber;
-
-                        numbersArray = tempNumbersArray;
-                        Console.WriteLine($"\nЧисло {inputNumber} было добавлено в массив.\n");
-                        break;
-                }
+                default:
+                    if (IsNumber(userInput, out int inputNumber))
+                    {
+                        AddNumber(numbers, inputNumber);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n{userInput} - неизвестная команда и не является числом.");
+                    }
+                    break;
             }
+        }
+
+        static void GetAmount(List<int> numbers)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine($"\nСумма введеных чисел: {sum}");
+        }
+
+        static bool IsNumber(string userInput, out int inputNumber)
+        {
+            if (int.TryParse(userInput, out inputNumber))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void AddNumber(List<int> numbers, int inputNumber)
+        {
+            numbers.Add(inputNumber);
+
+            Console.WriteLine($"\nЧисло {inputNumber} было добавлено.");
         }
     }
 }
