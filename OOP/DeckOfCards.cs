3efa29877a2779ba1214136b7c9493b1c940bafc
Anton Kovalenko <anton.saha@mail.ru>
2022-08-25 17:41:09 +0300

    added enums for playing cards

--- OOP/DeckOfCards.cs
+++ OOP/DeckOfCards.cs
@@ -14,6 +14,34 @@ namespace OOP
 
         class Card
         {
+            public Rank Rank { get; set; }
+
+            public Suit Suit { get; set; }
+        }
+
+        enum Rank
+        {
+            Rank_2,
+            Rank_3,
+            Rank_4,
+            Rank_5,
+            Rank_6,
+            Rank_7,
+            Rank_8,
+            Rank_9,
+            Rank_10,
+            Jack,
+            Queen,
+            King,
+            Ace,
+        }
+
+        enum Suit
+        {
+            Clubs,
+            Diamonds,
+            Hearts,
+            Spades,
         }
     }
 }
