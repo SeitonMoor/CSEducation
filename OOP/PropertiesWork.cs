faa20622f12210fde1e36f786adf24f0683c8580
Anton Kovalenko <anton.saha@mail.ru>
2022-08-27 19:07:48 +0300

    Created classes for working with properties

--- OOP/PropertiesWork.cs
+++ OOP/PropertiesWork.cs
@@ -4,16 +4,42 @@ namespace OOP
 {
     internal class PropertiesWork
     {
+        static void Main(string[] args)
+        {
+            Renderer renderer = new Renderer();
+
+            Player player = new Player(12, 19);
+            
+            renderer.DrawCharacter(player);
+        }
+
         class Player
         {
             int positionX;
             int positionY;
+
+            public Player(int positionX, int positionY)
+            {
+                this.positionX = positionX;
+                this.positionY = positionY;
+            }
+
+            public int GetPositionX()
+            {
+                return positionX;
+            }
+
+            public int GetPositionY()
+            {
+                return positionY;
+            }
         }
 
         class Renderer
         {
-            static void DrawCharacter()
+            public void DrawCharacter(Player player)
             {
+                Console.WriteLine($"Игрок отрисован по координатам: {player.GetPositionX()}.{player.GetPositionY()}");
             }
         }
     }
