b3c01bf0159ce5eff003952953ce83ba6ccf253a
Anton Kovalenko <anton.saha@mail.ru>
2022-08-28 23:17:43 +0300

    Created database of players with add, delete, ban, unban functions

--- OOP/PlayerDatabase.cs
+++ OOP/PlayerDatabase.cs
@@ -1,25 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP
 {
     internal class PlayerDatabase
     {
+        static void Main(string[] args)
+        {
+            Database database = new Database();
+            Player som = new Player(0, 2, "som");
+            Player trex = new Player(1, 13, "trexxx");
+            Player klr = new Player(2, 78, "klr");
+
+            database.Add(som);
+            database.Add(trex);
+            database.Add(klr);
+
+            database.Ban(trex);
+            database.Ban(klr);
+
+            database.Unban(klr);
+
+            database.Delete(trex);
+        }
+
         class Database
         {
-            static void Add()
+            List<Player> playerDb = new List<Player>();
+
+            public void Add(Player player)
             {
+                playerDb.Add(player);
             }
 
-            static void Ban()
+            public void Ban(Player player)
             {
+                player.SetIsBanned(true);
             }
 
-            static void Unban()
+            public void Unban(Player player)
             {
+                player.SetIsBanned(false);
             }
 
-            static void Delete()
+            public void Delete(Player player)
             {
+                playerDb.Remove(player);
             }
         }
 
@@ -30,8 +56,17 @@ namespace OOP
             string nickname;
             bool isBanned;
 
-            static void PrintInformation()
+            public Player(int id, int level, string nickname, bool isBanned = false)
             {
+                this.id = id;
+                this.level = level;
+                this.nickname = nickname;
+                this.isBanned = isBanned;
+            }
+
+            public void SetIsBanned(bool isBanned)
+            {
+                this.isBanned = isBanned;
             }
         }
     }
