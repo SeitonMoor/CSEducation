c0be2e32be589efc39adf38b798fa1953ef7e495
Anton Kovalenko <anton.saha@mail.ru>
2022-09-04 21:18:45 +0300

    Created a draft for passenger train configurator

--- OOP/PassengerTrainConfigurator.cs
+++ OOP/PassengerTrainConfigurator.cs
@@ -1,12 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOP
 {
     internal class PassengerTrainConfigurator
     {
+        static void Main(string[] args)
+        {
+        }
+
+        class Station
+        {
+
+            public void SelectCommand()
+            {
+                switch(1)
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    case 4:
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            public void CreateDirection()
+            {
+
+            }
+
+            public int SellTickets()
+            {
+                int purchasedTickets = 0;
+
+                return purchasedTickets;
+            }
+
+            public void FormTrain()
+            {
+
+            }
+
+            public void SendTrain()
+            {
+
+            }
+        }
+
+        class Train
+        {
+            
+        }
+
+        class Direction
+        {
+            string startStation;
+            string endStation;
+        }
+
+        class Passenger
+        {
+
+        }
     }
 }
