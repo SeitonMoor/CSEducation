ce5d120a84b44fa86d3a54bb293668a69fb65634
Anton Kovalenko <anton.saha@mail.ru>
2022-09-11 21:18:23 +0300

    Created the war field between factions

--- OOP/War.cs
+++ OOP/War.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOP
 {
     internal class War
     {
+        static void Main(string[] args)
+        {
+        }
+
+        class Solder
+        {
+            private int _health;
+            private int _maxHealth;
+            private int _damage;
+            private int _force;
+
+            public Solder(int health, int maxHealth, int damage)
+            {
+                _health = health;
+                _maxHealth = maxHealth;
+                _damage = damage;
+            }
+        }
+
+        class Troop
+        {
+            private List<Solder> _solders = new List<Solder>();
+
+            public Troop()
+            {
+            }
+        }
+
+        class Faction
+        {
+            private string _name;
+
+            public Faction (string name)
+            {
+                this._name = name;
+            }
+        }
     }
 }
