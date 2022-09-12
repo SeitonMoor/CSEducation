73625507f3602d17bd2eb176527e16fd0c4750bf
Anton Kovalenko <anton.saha@mail.ru>
2022-09-12 23:02:12 +0300

    Created the fish tank with fishes

--- OOP/Aquarium.cs
+++ OOP/Aquarium.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOP
 {
     internal class Aquarium
     {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+            }
+        }
+
+        class FishTank
+        {
+            private List<Fish> _fishes = new List<Fish>();
+            private int _maxSpace;
+
+            public FishTank(int maxSpace)
+            {
+                _maxSpace = maxSpace;
+            }
+        }
+
+        class Fish
+        {
+            private int _age;
+            private int _maxAge;
+
+            public Fish(int age, int maxAge)
+            {
+                _age = age;
+                _maxAge = maxAge;
+            }
+        }
     }
 }
