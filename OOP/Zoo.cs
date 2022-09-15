0fedacc0e75e889a48b631805f03cfd4573b9b19
Anton Kovalenko <anton.saha@mail.ru>
2022-09-15 23:57:36 +0300

    minor changing in a zoo while learn new information

--- OOP/Zoo.cs
+++ OOP/Zoo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP
 {
@@ -13,12 +14,30 @@ namespace OOP
 
         class Cage
         {
+            private string _name;
+            private int _animalCount;
+            private List<Animal> _animals = new List<Animal>();
 
+            public Cage(string name, int animalCount, List<Animal> animals)
+            {
+                _name = name;
+                _animalCount = animalCount;
+                _animals = animals;
+            }
         }
 
         class Animal
         {
+            private string _name;
+            private string _sound;
+            private string _gender;
 
+            public Animal(string name, string sound, string gender)
+            {
+                _name = name;
+                _sound = sound;
+                _gender = gender;
+            }
         }
     }
 }
