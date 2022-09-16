a2f9c4627564c2a1cbf3a0ed2ca6ebebdef528f7
Anton Kovalenko <anton.saha@mail.ru>
2022-09-16 23:29:26 +0300

    minor changing in a car service while learn new information

--- OOP/CarService.cs
+++ OOP/CarService.cs
@@ -12,16 +12,31 @@ namespace OOP
         class Service
         {
             private List<Detail> _details = new List<Detail>();
+            private int _balance;
         }
 
         class Detail
         {
             private string _name;
+            private int _price;
+
+            public Detail(string name, int price)
+            {
+                _name = name;
+                _price = price;
+            }
         }
 
-        class Client
+        class Car
         {
+            private string _name;
+            private string _damage;
 
+            public Car(string name, string damage)
+            {
+                _name = name;
+                _damage = damage;
+            }
         }
     }
 }
