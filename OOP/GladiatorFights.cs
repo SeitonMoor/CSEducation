f32c01a0b260a7b6fdf2db30b2a2fb493e1c17fa
Anton Kovalenko <anton.saha@mail.ru>
2022-09-26 22:31:07 +0300

    Created a control menu for working with a gladiator stage

--- OOP/GladiatorFights.cs
+++ OOP/GladiatorFights.cs
@@ -6,6 +6,37 @@ namespace OOP
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Гладиаторские бои");
+            Console.Write("\nВы можете:" +
+                    "\n\n1 - выбрать бойца №1 - Воин." +
+                    "\n2 - выбрать бойца №2 - Маг." +
+                    "\n3 - выбрать бойца №3 - Стрелок." +
+                    "\n4- выбрать бойца №4 - Паладин." +
+                    "\n5 - выбрать бойца №5 - Друид." +
+                    "\n\nВаш выбор: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    break;
+
+                case "2":
+                    break;
+
+                case "3":
+                    break;
+
+                case "4":
+                    break;
+
+                case "5":
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.Clear();
         }
 
         class Fighter
