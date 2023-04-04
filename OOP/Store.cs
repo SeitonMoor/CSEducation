64c2506dc209cf236f98dc12b0a0ef16d62f1dfe
Anton Kovalenko <anton.saha@mail.ru>
2023-04-04 23:44:24 +0300

    Added constants, properties and created a products fill

--- OOP/Store.cs
+++ OOP/Store.cs
@@ -5,9 +5,14 @@ namespace OOP
 {
     internal class Store
     {
+        private const string SeeCommand = "see";
+        private const string BuyCommand = "buy";
+        private const string ViewCommand = "view";
+        private const string ExitCommand = "exit";
+
         static void Main(string[] args)
         {
-            Player player = new Player();
+            Buyer buyer = new Buyer();
             Seller seller = new Seller();
             bool isWorking = true;
 
@@ -15,27 +20,27 @@ namespace OOP
             {
                 Console.WriteLine("Магазин");
                 Console.Write("\nВы можете:" +
-                    "\n\nsee - посмотреть список товаров." +
-                    "\nbuy - купить товар." +
-                    "\nview - посмотреть купленные товары." +
-                    "\nexit - закончить покупку." +
+                    $"\n\n{SeeCommand} - посмотреть список товаров." +
+                    $"\n{BuyCommand} - купить товар." +
+                    $"\n{ViewCommand} - посмотреть купленные товары." +
+                    $"\n{ExitCommand}- закончить покупку." +
                     "\n\nВаш выбор: ");
 
                 switch (Console.ReadLine())
                 {
-                    case "see":
+                    case SeeCommand:
                         seller.ShowProducts();
                         break;
 
-                    case "buy":
-                        seller.SellProduct(player);
+                    case BuyCommand:
+                        seller.SellProduct(buyer);
                         break;
 
-                    case "view":
-                        player.ViewInventory();
+                    case ViewCommand:
+                        buyer.ViewInventory();
                         break;
 
-                    case "exit":
+                    case ExitCommand:
                         isWorking = false;
                         break;
 
@@ -49,15 +54,22 @@ namespace OOP
             }
         }
 
-        class Player
+        class Buyer
         {
             private List<Product> _items = new List<Product>();
 
             public void ViewInventory()
             {
-                foreach (Product item in _items)
+                if (_items.Count == 0)
                 {
-                    Console.WriteLine($"{item.GetName()} по цене {item.GetPrice()}, в наличии: {item.GetCout()}");
+                    Console.WriteLine("Ваш инвентарь пуст.");
+                }
+                else
+                {
+                    foreach (Product item in _items)
+                    {
+                        Console.WriteLine($"{item.Name} по цене {item.Price}, в наличии: {item.Count}");
+                    }
                 }
             }
 
@@ -67,7 +79,7 @@ namespace OOP
 
                 foreach (Product item in _items)
                 {
-                    if (product.GetName() == item.GetName())
+                    if (product.Name == item.Name)
                     {
                         item.AddCount(count);
 
@@ -88,38 +100,33 @@ namespace OOP
 
             public Seller()
             {
-                _products.Add(new Product("Арбуз", 120, 11));
-                _products.Add(new Product("Апельсин", 60, 99));
-                _products.Add(new Product("Яблоко", 150, 210));
-                _products.Add(new Product("Виноград", 50, 45));
-                _products.Add(new Product("Кофе", 340, 6));
-                _products.Add(new Product("Миндаль", 330, 12));
+                FillProducts();
             }
 
             public void ShowProducts()
             {
                 foreach (Product product in _products)
                 {
-                    Console.WriteLine($"{product.GetName()} по цене {product.GetPrice()}, в наличии: {product.GetCout()}");
+                    Console.WriteLine($"{product.Name} по цене {product.Price}, в наличии: {product.Count}");
                 }
             }
 
-            public void SellProduct(Player player)
+            public void SellProduct(Buyer player)
             {
                 Product product = GetProduct();
                 bool isSold = false;
 
-                do
+                while (isSold == false)
                 {
-                    Console.Write($"Напишите количество {product.GetName()} для покупки: ");
+                    Console.Write($"Напишите количество {product.Name} для покупки: ");
 
                     if (Int32.TryParse(Console.ReadLine(), out int count))
                     {
-                        if (product.GetCout() < count)
+                        if (product.Count < count)
                         {
-                            Console.WriteLine($"Данного количества товара нет в наличии, максимально возможное - {product.GetCout()}.\n");
+                            Console.WriteLine($"Данного количества товара нет в наличии, максимально возможное - {product.Count}.\n");
                         }
-                        else if (product.GetCout() == count)
+                        else if (product.Count == count)
                         {
                             player.TakeItem(product, count);
 
@@ -131,7 +138,7 @@ namespace OOP
                         {
                             product.ReduceCount(count);
 
-                            Product playerProduct = new Product(product.GetName(), product.GetPrice(), count);
+                            Product playerProduct = new Product(product.Name, product.Price, count);
 
                             player.TakeItem(playerProduct, count);
 
@@ -143,7 +150,6 @@ namespace OOP
                         Console.WriteLine("Количество указано не верно.\n");
                     }
                 }
-                while (isSold == false);
             }
 
             private Product GetProduct()
@@ -151,62 +157,56 @@ namespace OOP
                 Product foundProduct = null;
                 bool isFound = false;
 
-                do
+                while (isFound == false)
                 {
                     Console.Write("Выберите продукт для покупки: ");
                     string name = Console.ReadLine();
 
                     foreach (Product product in _products)
                     {
-                        if (product.GetName() == name)
+                        if (product.Name == name)
                         {
                             foundProduct = product;
                             isFound = true;
                         }
                     }
                 }
-                while (isFound == false);
 
                 return foundProduct;
+            }
+
+            private void FillProducts()
+            {
+                _products.Add(new Product("Арбуз", 120, 11));
+                _products.Add(new Product("Апельсин", 60, 99));
+                _products.Add(new Product("Яблоко", 150, 210));
+                _products.Add(new Product("Виноград", 50, 45));
+                _products.Add(new Product("Кофе", 340, 6));
+                _products.Add(new Product("Миндаль", 330, 12));
             }
         }
 
         class Product
         {
-            private string _name;
-            private int _price;
-            private int _count;
-
             public Product(string name, int price, int count)
             {
-                _name = name;
-                _price = price;
-                _count = count;
+                Name = name;
+                Price = price;
+                Count = count;
             }
 
-            public string GetName()
-            {
-                return _name;
-            }
-
-            public int GetPrice()
-            {
-                return _price;
-            }
-
-            public int GetCout()
-            {
-                return _count;
-            }
+            public string Name { get; private set; }
+            public int Price { get; private set; }
+            public int Count { get; private set; }
 
             public void AddCount(int count)
             {
-                _count += count;
+                Count += count;
             }
 
             public void ReduceCount(int count)
             {
-                _count -= count;
+                Count -= count;
             }
         }
     }
