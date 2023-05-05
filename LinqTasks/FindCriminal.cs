6bd4f441cc0d2bfe83959c8bbcf6e7abe5f9a8ba
Anton Kovalenko <anton.saha@mail.ru>
2023-05-05 23:30:33 +0300

    Added enums for forming a random criminal and planned a classes

--- LinqTasks/FindCriminal.cs
+++ LinqTasks/FindCriminal.cs
@@ -6,6 +6,62 @@ namespace LinqTasks
     {
         static void Main(string[] args)
         {
+            Interpol interpol = new Interpol();
+            interpol.Work();
         }
+    }
+
+    enum Surname
+    {
+        Smith,
+        Brown,
+        Roy,
+        Wilson,
+        Petrov
+    }
+
+    enum Name
+    {
+        William,
+        Ivan,
+        John,
+        Robert,
+        Henry,
+        Thomas,
+        Petr,
+        David,
+        Alex
+    }
+
+    enum Patronymic
+    {
+        Adamson,
+        Dixon,
+        Wilson,
+        Thompson,
+        Ivanov,
+        Petrov,
+        Gibson,
+        Emberson
+    }
+
+    enum Nation
+    {
+        British,
+        American,
+        Russian,
+        Australian,
+        Canadian,
+        German,
+        Italian
+    }
+
+    class Interpol
+    {
+        public void Work() { }
+    }
+
+    class Criminal
+    {
     }
 }
